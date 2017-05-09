94800ef2a2ccef2f3058661e20443904b708b879
Hemant Goutam <hemant1906@yahoo.com>
2017-05-09 09:14:33 +0530

    Added additional sites for testing

--- AKKA_Crawler/Actors/SiteCrawler.cs
+++ AKKA_Crawler/Actors/SiteCrawler.cs
@@ -13,20 +13,20 @@ namespace AKKA_Crawler.Actors
         int FailedPages = 0;
         string _site;
         Dictionary<string, IActorRef> childActors = new Dictionary<string, IActorRef>();
-      
+
         private void CreateChidActor(string childUrl)
         {
-			var pageScrapper = Context.ActorOf(Props.Create<PageScrapper>());
+            var pageScrapper = Context.ActorOf(Props.Create<PageScrapper>());
             childActors.Add(childUrl, pageScrapper);
-            pageScrapper.Tell(new StartScrapping(childUrl));			
+            pageScrapper.Tell(new StartScrapping(childUrl));
             PagesToScrap[childUrl] += 1;
         }
 
         protected override SupervisorStrategy SupervisorStrategy()
         {
-            return new  OneForOneStrategy(
+            return new OneForOneStrategy(
                 maxNrOfRetries: 3,
-                withinTimeRange:TimeSpan.FromSeconds(30),
+                withinTimeRange: TimeSpan.FromSeconds(30),
                 decider: Decider.From(x =>
                     {
                         if (x is AggregateException)
@@ -35,99 +35,95 @@ namespace AKKA_Crawler.Actors
                             Console.WriteLine("Exception occured processing a child. Stopping");
                             Console.ResetColor();
                             return Directive.Resume; // We could have checked inner exception but for simplicity we are keeping it
-
-
                         }
                         else
                         {
-
                             Console.ForegroundColor = ConsoleColor.Red;
-                            
+
                             Console.WriteLine("Exception occured processing a child. Restarting");
                             Console.ResetColor();
                             return Directive.Restart;
                         }
                     }
                 )
-                
+
                 );
         }
 
 
         protected override void OnReceive(object message)
         {
-            if(message is StartScrapping)
+            
+
+            if (message is StartScrapping)
             {
                 var scrapMessage = message as StartScrapping;
                 AddPage(scrapMessage.PageToScrap);
                 CreateChidActor(scrapMessage.PageToScrap);
 
-            Console.ResetColor();
-                Console.WriteLine("{0} Started for {1} times",scrapMessage.PageToScrap,PagesToScrap[scrapMessage.PageToScrap]);
+                Console.ResetColor();
+                Console.WriteLine("{0} Started for {1} times", scrapMessage.PageToScrap, PagesToScrap[scrapMessage.PageToScrap]);
             }
             else if (message is CrawlSite)
             {
-                
-				var scrapMessage = message as CrawlSite;
+
+                var scrapMessage = message as CrawlSite;
                 _site = scrapMessage.SiteUrl;
                 AddPage(scrapMessage.SiteUrl);
                 CreateChidActor(scrapMessage.SiteUrl);
                 PagesToScrap[scrapMessage.SiteUrl] += 1;
 
-            Console.ResetColor();
+                Console.ResetColor();
                 Console.WriteLine("{0} Started for {1} times", scrapMessage.SiteUrl, PagesToScrap[scrapMessage.SiteUrl]);
             }
             else if (message is PageScrapped)
-                {
+            {
                 ScrappedPages++;
-			var scrappedPage = message as PageScrapped;
-			Console.WriteLine("{0} Scrap Complete", scrappedPage.PageName);
+                var scrappedPage = message as PageScrapped;
+                Console.WriteLine("{0} Scrap Complete", scrappedPage.PageName);
 
                 Context.Stop(childActors[scrappedPage.PageName]);
 
-                try
+
+                int childPages = 0;
+
+                foreach (var url in scrappedPage.ChildPages)
                 {
-                    int childPages = 0;
-
-                    foreach(var url in scrappedPage.ChildPages)
+                    if (AddPage(url))
                     {
-                        if(AddPage(url))
-                        {
-                            childPages++;
-                            CreateChidActor(url);
-                        }
+                        childPages++;
+                        CreateChidActor(url);
                     }
-
+                }
 
                 Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine("Page {0} Scrapped. It has {1} links", scrappedPage.PageName, childPages);
+                Console.WriteLine("Page {0} Scrapped. It has {1} links", scrappedPage.PageName, childPages);
                 Console.ResetColor();
-                }catch(Exception )
-                {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("Unable to process child for {0}", scrappedPage.PageName);  
-                Console.ResetColor();
-                }
-
-                }
-                else if (message is ScrappingError)
-                {
+            }
+            else if (message is ScrappingError)
+            {
                 var msg = message as ScrappingError;
-                if(  PagesToScrap[msg.Url] <= 5)  // try 5 times
+                if (PagesToScrap[msg.Url] <= 5)  // try 5 times
                 {
-                Console.ForegroundColor = ConsoleColor.Red;   
-                    Console.WriteLine("Error scrapping {0}. Trying again",msg.Url);
-                Console.ResetColor();
-                }else
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Error scrapping {0}. Trying again", msg.Url);
+                    Console.ResetColor();
+                    childActors[msg.Url].Tell(new StartScrapping(msg.Url));
+                    PagesToScrap[msg.Url] += 1;
+                }
+                else
                 {
                     Console.ForegroundColor = ConsoleColor.Red;
                     Console.WriteLine("Error scrapping {0}. Stopping", msg.Url);
                     Console.ResetColor();
                     Context.Stop(childActors[msg.Url]);
-                        
+
                     FailedPages++;
                 }
-                }
+            }
+
+            Console.WriteLine(PagesToScrap.Count - (ScrappedPages + FailedPages));// debugging only need to remove it
+
 
             if ((ScrappedPages + FailedPages) == PagesToScrap.Count)
                 Context.Parent.Tell(new CrawlComplete(_site));
@@ -136,17 +132,17 @@ namespace AKKA_Crawler.Actors
         private bool AddPage(string page)
         {
             bool added = false;
-            if(!PagesToScrap.ContainsKey(page))
+            if (!PagesToScrap.ContainsKey(page))
             {
                 if (Uri.IsWellFormedUriString(page, UriKind.Absolute))
                 {
                     PagesToScrap.Add(page, 0);
-        
+
                     added = true;
                 }
             }
             return added;
-                
+
         }
 
     }
